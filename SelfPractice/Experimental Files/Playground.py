6d6444d8ef4b68fd10e840af2c20cd42dc5f824d
Ncjdod <kaankuzubasioglu@gmail.com>
2026-01-23 17:03:45 +0100

    commit

--- SelfPractice/Experimental Files/Playground.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-sizes = [2, 3, 1]  # Example sizes, you can change these values
-weights = [np.random.randn(y, x) 
-                        for x, y in zip(sizes[:-1], sizes[1:])]
-print(weights[1])
-print(weights)
-
